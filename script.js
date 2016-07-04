9e3ce491c508d8953819e4a11534e7fe7562c619
Japheth Balane <japheth162@gmail.com>
2016-07-04 10:43:19 +0800

    Added snow effect; Improved tree implementation;

--- script.js
+++ script.js
@@ -4,19 +4,67 @@ var context = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var line = [];
-var snow = [];
+//////////////////////////////////////////////////////////////////////////////////////
+
+var lines = [];
+var snows = [];
 var leaves = [];
 
-var ctr = 0;
+var maxLineCount = 150;
 
-var level = 10;
-var flag = true;
+//////////////////////////////////////////////////////////////////////////////////////
 
-clearCanvas(); 
-// generateSnow(10);
 generateLine();
-setInterval(drawTree, 30);
+generateSnow(100);
+generateLeaf();
+
+setInterval(draw, 30);
+// draw();
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+function generateLine() {
+	for (var i = 0; i < maxLineCount; i++) {
+		lines.push(new Line());
+	}
+	initRoot();
+	initChildren();
+}
+
+function generateSnow(count) {
+	for (var i = 0; i < count; i++) {
+		snows.push(new Snow());
+	}
+}
+
+function generateLeaf() {
+	for (var i = 4; i < lines.length; i++) {
+		leaves.push(new Leaf(lines[i].x2,lines[i].y2));
+	}
+}
+
+function initRoot() {
+	lines[1].parent = null;
+	lines[1].x1 = canvas.width/2;
+	lines[1].y1 = canvas.height*4/5+20;
+	lines[1].length = 100;
+	lines[1].angle = 270;
+	lines[1].x2 = canvas.width/2;
+	lines[1].y2 = lines[1].y1-lines[1].length;
+	lines[1].lineWidth = 5;
+}
+
+function initChildren() {
+	for (var i = 1; i < lines.length/2; i++) {
+		lines[i*2].parent = lines[i];
+		lines[i*2+1].parent = lines[i];
+	}
+	for (var i = 2; i < lines.length; i++) {
+		lines[i].init();
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
 
 function randomBetween(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
@@ -26,96 +74,57 @@ function clearCanvas() {
 	context.fillStyle = "#222222";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	context.fillStyle = "#dddddd";
-	context.fillRect(0, canvas.height-100, canvas.width, 100);
+	context.fillRect(0, canvas.height*4/5, canvas.width, canvas.height*1/5);
 }
 
-function trunk() {
-	context.beginPath();
-	context.moveTo(line[0].x1+1, line[0].y1+1);
-	context.lineTo(line[0].x2+1, line[0].y2-1);
-	context.strokeStyle = "#fff";
-	context.stroke();
-}
+//////////////////////////////////////////////////////////////////////////////////////
 
-function generateSnow(count) {
-	for (var i = 0; i < count; i++) {
-		snow.push(new Snow());
-	}
-}
-
-function generateLeaf(x,y) {
-	leaves.push(new Leaf(x,y));
-}
-
-function generateLine() {
-	line.push(new Line(canvas.width/2, canvas.height-100, 260, 280, 150));
-	// line.push(new Line(canvas.width/2+300, canvas.height-100, 260, 280, 150));
-}
-
-function drawTree() {
+function draw() {
 	clearCanvas();
-	trunk();
-	for (var i = 0; i < snow.length; i++) {
-		snow[i].update().draw();
+	for (var i = 1; i < lines.length; i++) {
+		lines[i].update().draw();
 	}
-	for (var i = 0; i < line.length; i++) {
-		line[i].update().draw();
+	for (var i = 0; i < snows.length; i++) {
+		snows[i].update().draw();
 	}
-	// for (var i = 0; i < leaves.length; i++) {
-	// 	leaves[i].update().draw();
-	// }
-	
-	// flag = true;
+	for (var i = 0; i < leaves.length; i++) {
+		leaves[i].update().draw();
+	}
 }
 
-function Line(x,y,ang1,ang2,life) {
-	this.x1 = x;
-	this.y1 = y;
-	this.x2 = this.x1;
-	this.y2 = this.y1;
-	this.angle = randomBetween(ang1,ang2);
-	this.speed = 5;
-	this.life = life;
-	this.flag = true;
+//////////////////////////////////////////////////////////////////////////////////////
 
-	if (life <= 50) {
-		this.flag = false;
-	};
+function Line() {
+	this.parent;
+	this.x1;
+	this.y1;
+	this.x2;
+	this.y2;
+	this.length;
+	this.lineWidth;
+	this.angle;
+
+	this.init = function() {
+		this.x1 = this.parent.x2;
+		this.y1 = this.parent.y2;
+		this.length = this.parent.length/1.5;
+		this.angle = 270 + randomBetween(-90,90);
+		this.x2 = this.x1 + Math.cos(this.angle * Math.PI / 180) * this.length;
+		this.y2 = this.y1 - this.length + randomBetween(-20,20);
+		this.lineWidth = this.parent.lineWidth / 1.3;
+	}
 
 	this.update = function() {
-		this.life--;
-    	if (this.life > 50) {
-    		var dx = Math.cos(this.angle * Math.PI / 180);
-    		var dy = Math.sin(this.angle * Math.PI / 180);
-    		this.x2 += dx;
-    		this.y2	+= dy;
-    	};
-    	if (this.life <= 50 && this.flag) {
-    		var rand = randomBetween(5, 10);
-    		for (var i = 0; i < rand; i++) {
-    			line.push(new Line(this.x2, this.y2, ang1-40, ang2+40, life-20));
-    			if (ctr == 1) {
-    				generateLeaf(this.x2, this.y2);
-    			};
-    		};
-    		ctr = 1;
-    		level--;
-    		this.flag = false;
-    		// console.log(rand);
-    		// flag = false;
-    	};
-
 		return this;
 	}
 
 	this.draw = function() {
 		context.beginPath();
-		context.moveTo(this.x1, this.y1);
-		context.lineTo(this.x2, this.y2);
-		context.strokeStyle = "#ffffff";
+		context.lineCap = "round";
+		context.lineWidth = this.lineWidth;
+		context.moveTo(this.x1,this.y1);
+		context.lineTo(this.x2,this.y2);
 		context.stroke();
-
-		return this;
 	}
 }
 
@@ -123,22 +132,14 @@ function Snow() {
 	this.x = randomBetween(0, canvas.width);
 	this.y = randomBetween(0, canvas.height);
 	this.radius = 1;
-	this.windx = randomBetween(-1,2);
-	this.windy = randomBetween(1,3);
+	this.speed = randomBetween(1,3);
 
 	this.update = function() {
-		this.y+=this.windy;
-		this.x-=this.windx;
-
-		if (this.x < 0) {
-			this.x = canvas.width;
-		};
-
+		this.y += this.speed;
 		if (this.y > canvas.height) {
 			this.x = randomBetween(0, canvas.width);
 			this.y = 0;
 		};
-
 		return this;
 	}
 
@@ -148,6 +149,10 @@ function Snow() {
 		context.fillStyle = "#ffffff";
 		context.fill();
 
+		context.beginPath();
+		context.arc(this.x, this.y, this.radius*100, Math.PI * 2, false);
+		context.fillStyle = "rgba(255,255,255,0.01)";
+		context.fill();
 		return this;
 	}
 }
@@ -155,32 +160,53 @@ function Snow() {
 function Leaf(x,y) {
 	this.x = x;
 	this.y = y;
-	this.radius = 10;
-	this.r = randomBetween(0,0);
-	this.g = randomBetween(100,255);
-	this.b = randomBetween(0,0);
-	// this.fall = false;
+	this.radius = 30;
+	this.r = randomBetween(250,255);
+	this.g = randomBetween(250,255);
+	this.b = randomBetween(250,255);
 
 	this.update = function() {
-		// if (!this.fall) {
-		// 	var f = randomBetween(1, 5000);
-		// 	if (f == 1) {
-		// 		this.fall = true;
-		// 	};
-		// };
-		// if (this.fall) {
-		// 	if (this.y <= canvas.height-100) {
-		// 		this.y++;
-		// 	};
-		// };
-
 		return this;
 	}
 
 	this.draw = function() {
+		// context.beginPath();
+		// context.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+		// context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",0.05)";
+		// context.fill();
+
 		context.beginPath();
-		context.arc(this.x, this.y, this.radius, Math.PI * 2, false);
-		context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",0.1)";
+		context.arc(this.x+30, this.y, this.radius/3, Math.PI * 2, false);
+		context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",0.04)";
+		context.fill();
+		context.beginPath();
+		context.arc(this.x-30, this.y, this.radius/3, Math.PI * 2, false);
+		context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",0.04)";
+		context.fill();
+		context.beginPath();
+		context.arc(this.x, this.y+30, this.radius/3, Math.PI * 2, false);
+		context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",0.04)";
+		context.fill();
+		context.beginPath();
+		context.arc(this.x, this.y-30, this.radius/3, Math.PI * 2, false);
+		context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",0.04)";
+		context.fill();
+
+		context.beginPath();
+		context.arc(this.x+50, this.y-50, this.radius/3, Math.PI * 2, false);
+		context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",0.04)";
+		context.fill();
+		context.beginPath();
+		context.arc(this.x-50, this.y+50, this.radius/3, Math.PI * 2, false);
+		context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",0.04)";
+		context.fill();
+		context.beginPath();
+		context.arc(this.x+50, this.y+50, this.radius/3, Math.PI * 2, false);
+		context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",0.04)";
+		context.fill();
+		context.beginPath();
+		context.arc(this.x-50, this.y-50, this.radius/3, Math.PI * 2, false);
+		context.fillStyle = "rgba("+this.r+","+this.g+","+this.b+",0.04)";
 		context.fill();
 	}
 }
